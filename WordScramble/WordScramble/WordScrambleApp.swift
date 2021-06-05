e970fa59462c20bddf39171ff2cb8c9c0c956b16
peterkost <46546031+peterkost@users.noreply.github.com>
2021-06-05 15:27:53 +0300

    project 5
    days 29-31

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Peter Kostin on 2021-06-05.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
