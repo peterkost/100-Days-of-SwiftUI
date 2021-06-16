69af6c4babb2c6951cf9025d190143652677f9bd
peterkost <46546031+peterkost@users.noreply.github.com>
2021-06-16 10:40:32 +0300

    Project 17
    Days 86-91.
    Only Challenge 1 completed.

---
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -0,0 +1,240 @@
+//
+//  ContentView.swift
+//  Flashzilla
+//
+//  Created by Peter Kostin on 2021-06-15.
+//
+
+import SwiftUI
+import CoreHaptics
+
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = CGFloat(total - position)
+        return self.offset(CGSize(width: 0, height: offset * 10))
+    }
+}
+
+struct ContentView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @Environment(\.accessibilityEnabled) var accessibilityEnabled
+    @State private var cards = [Card]()
+    @State private var showingEditScreen = false
+    @State private var engine: CHHapticEngine?
+    @State private var timerEnd = false
+    @State private var reuse = true
+
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State private var isActive = true
+    
+     
+    var body: some View {
+        ZStack {
+            Image(decorative: "background")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea(.all)
+            
+            VStack {
+                Text(timerEnd ? "Time's Up!" : "Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(
+                        Capsule()
+                            .fill(Color.black)
+                            .opacity(0.75)
+                    )
+                    .onAppear(perform: prepareHaptics)
+
+
+                
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index]) {
+                            withAnimation {
+                                removeCard(at: index)
+                            }
+                        }
+                        .stacked(at: index, in: cards.count)
+                        .allowsHitTesting(index == cards.count - 1)
+                        .accessibility(hidden: index < cards.count - 1)
+                    }
+                }
+                .allowsHitTesting(timeRemaining > 0)
+            
+                if cards.isEmpty {
+                    Button("Start Again", action: resetCards)
+                        .padding()
+                        .background(Color.white)
+                        .foregroundColor(.black)
+                        .clipShape(Capsule())
+                }
+            }
+            
+            VStack {
+                HStack {
+                    // Not sure if it's a bug, but a single spacer pushes the button off screen.
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Button(action: {
+                        showingEditScreen = true
+                    }) {
+                        Image(systemName: "plus.circle")
+                            .padding()
+                            .background(Color.black.opacity(0.7))
+                            .clipShape(Circle())
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
+            .foregroundColor(.white)
+            .font(.largeTitle)
+            .padding()
+            
+            if differentiateWithoutColor || accessibilityEnabled {
+                VStack {
+                    Spacer()
+                    
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            withAnimation {
+                                removeCard(at: cards.count - 1)
+                            }
+                        }) {
+                            Image(systemName: "xmark.circle")
+                                .padding()
+                                .background(Color.black.opacity(0.7))
+                                .clipShape(Circle())
+                        }
+                        .accessibility(label: Text("Wrong"))
+                        .accessibility(hint: Text("Mark your answer as being incorrect."))
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        
+                        Button(action: {
+                            withAnimation {
+                                removeCard(at: cards.count - 1)
+                            }
+                        }) {
+                            Image(systemName: "checkmark.circle")
+                                .padding()
+                                .background(Color.black.opacity(0.7))
+                                .clipShape(Circle())
+                        }
+                        .accessibility(label: Text("Correct"))
+                        .accessibility(hint: Text("Mark your answer as being correct."))
+                        Spacer()
+                    }
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .padding()
+                }
+            }
+            
+        }
+        .onReceive(timer) { time in
+            guard isActive else { return }
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            } else {
+                if !timerEnd {
+                    timerEnd = true
+                    timerEndHaptic()
+                }
+            }
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
+            isActive = false
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+            if cards.isEmpty == false {
+                isActive = true
+            }
+        }
+        .sheet(isPresented: $showingEditScreen, onDismiss: resetCards) {
+            EditCards()
+        }
+        .onAppear(perform: resetCards)
+    }
+    
+    func removeCard(at index: Int) {
+        guard index >= 0 else { return }
+
+        cards.remove(at: index)
+        if cards.isEmpty {
+            isActive = false
+        }
+    }
+    
+//    func reuseCard(at index: Int) {
+//        guard index >= 0 else { return }
+//
+//        let card = cards[index]
+//        cards.remove(at: index)
+//        cards.insert(car)
+//    }
+    
+    func resetCards() {
+        timeRemaining = 100
+        timerEnd = false
+        isActive = true
+        loadData()
+    }
+    
+    func loadData() {
+        if let data = UserDefaults.standard.data(forKey: "Cards") {
+            if let decoded = try? JSONDecoder().decode([Card].self, from: data) {
+                cards = decoded
+            }
+        }
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+
+        do {
+            self.engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
+    
+    func timerEndHaptic() {
+        // make sure that the device supports haptics
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+
+        // create one intense, sharp tap
+        for i in stride(from: 0, to: 1.5, by: 0.5) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
+            events.append(event)
+        }
+        
+
+        // convert those events into a pattern and play it immediately
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Peter Kostin on 2021-06-15.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
